2129bef1385b0cd10d981e5b90a3d60cc522e9e3
Paulucha <paulina.przekop@gmail.com>
2018-09-05 13:06:30 +0200

    centuryFromYear

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+/**
+ Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100,
+ the second - from the year 101 up to and including the year 200, etc.
+
+ Example
+ For year = 1905, the output should be
+ centuryFromYear(year) = 20;
+ For year = 1700, the output should be
+ centuryFromYear(year) = 17.
+ */
+
+int centuryFromYear(int year) {
+
+    int sto = 100;
+    int century = year/sto;
+    if (year % sto==0){
+
+        return century;
+    }
+    else
+        return century+1;
+}
+
+
+
+}
