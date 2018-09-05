aedf2be7a7730b7113eaa935ad5a37e53530d589
Paulucha <paulina.przekop@gmail.com>
2018-09-05 15:20:36 +0200

    shapeArea

--- src/Main.java
+++ src/Main.java
@@ -69,5 +69,23 @@ public class Main {
         return max;
     }
 
+    /**
+     Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+      A 1-interesting polygon is just a square with a side of length 1.
+     n-interesting polygon is obtained by taking the n - 1-interesting polygon
+     and appending 1-interesting polygons to its rim, side by side.
+     You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+       */
+
+    int shapeArea(int n) {
+        int area;
+
+        if (n>1){
+            return area = ((4*(n-1))+(shapeArea(n-1)));
+        }
+        else
+            return area =1;
+    }
+
 
 }
