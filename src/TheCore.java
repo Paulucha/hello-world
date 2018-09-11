fb62337a7f86bff1ab03096beace63edcb563aa5
Paulucha <paulina.przekop@gmail.com>
2018-09-11 14:00:43 +0200

    matrixElementsSum

--- src/TheCore.java
+++ src/TheCore.java
@@ -25,6 +25,9 @@ public class TheCore {
         return sum;
     }
 
+    /**
+     Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+     */
     int firstDuplicate(int[] a) {
         if(a.length<=1) return -1;
         for(int i=0; i< a.length; i++){
