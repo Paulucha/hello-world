7dfb8bf32b9475280ffb4dae39a7aeafdc79b90b
Paulucha <paulina.przekop@gmail.com>
2018-09-12 15:55:20 +0200

    largestNumber

--- CodeSignal excercises/src/Main.java
+++ CodeSignal excercises/src/Main.java
@@ -9,5 +9,6 @@ public class Main {
 
         Intro.makeArrayConsecutive2(new int[]{2, 3, 6, 9});
         Intro.almostIncreasingSequence(new int[]{2,9, 6, });
+        System.out.println(TheCore.largestNumber(3));
     }
 }
--- CodeSignal excercises/src/TheCore.java
+++ CodeSignal excercises/src/TheCore.java
@@ -3,22 +3,22 @@ import java.util.List;
 
 public class TheCore {
     /**
-     You are given a two-digit integer n. Return the sum of its digits.
-
-     Example
-
-     For n = 29, the output should be
-     addTwoDigits(n) = 11.
+     * You are given a two-digit integer n. Return the sum of its digits.
+     * <p>
+     * Example
+     * <p>
+     * For n = 29, the output should be
+     * addTwoDigits(n) = 11.
      */
 
     int addTwoDigits(int n) {
         int sum = 0;
         List<Integer> digits = new ArrayList<>();
-        while (n>0){
-            digits.add(n%10);
-            n = n/10;
+        while (n > 0) {
+            digits.add(n % 10);
+            n = n / 10;
         }
-        for (int i =0; i < digits.size(); i++){
+        for (int i = 0; i < digits.size(); i++) {
             sum = sum + digits.get(i);
         }
 
@@ -26,16 +26,31 @@ public class TheCore {
     }
 
     /**
-     Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+     * Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
      */
     int firstDuplicate(int[] a) {
-        if(a.length<=1) return -1;
-        for(int i=0; i< a.length; i++){
-            int pos= Math.abs(a[i])-1;
-            if(a[pos]<0) return pos+1;
-            else a[pos]=-a[pos];
+        if (a.length <= 1) return -1;
+        for (int i = 0; i < a.length; i++) {
+            int pos = Math.abs(a[i]) - 1;
+            if (a[pos] < 0) return pos + 1;
+            else a[pos] = -a[pos];
         }
         return -1;
     }
+
+    /**
+     * Given an integer n, return the largest number that contains exactly n digits.
+     * Example
+     * For n = 2, the output should be
+     * largestNumber(n) = 99.
+     */
+   static int largestNumber(int n) {
+        double number=0;
+        for (int i = 0; i < n; i++) {
+            number = number + ((Math.pow(10,i)) *9);
+        }
+        return (int) number;
+    }
+
 
 }
