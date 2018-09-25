f75bb867d7a5dbb5fc60452b1f9dad5bf82d3ad6
Paulucha <paulina.przekop@gmail.com>
2018-09-25 13:22:01 +0200

    arithmeticExpression

--- CodeSignal excercises/src/TheCore.java
+++ CodeSignal excercises/src/TheCore.java
@@ -321,5 +321,18 @@ public class TheCore {
         } else return true;
     }
 
+    /**
+     * Consider an arithmetic expression of the form a#b=c. Check whether it is possible to replace # with one of the four signs: +, -, * or / to obtain a correct expression.
+     */
+
+    boolean arithmeticExpression(int a, int b, int c) {
+
+
+        if ((a + b == c) || (a * b == c) || (a / b == c) && a % b == 0 || (a - b == c)) {
+
+            return true;
+        } else return false;
+
+    }
 
 }
