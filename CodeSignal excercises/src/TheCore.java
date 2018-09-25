bdbdf09fb11fe691d19b4dddb610931453d28cf3
Paulucha <paulina.przekop@gmail.com>
2018-09-25 12:39:05 +0200

    extraNumber

--- CodeSignal excercises/src/TheCore.java
+++ CodeSignal excercises/src/TheCore.java
@@ -290,5 +290,20 @@ Note that there are only two items and you can't bring more than one item of eac
            return value1;
     }
 
+/**
+You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
+ */
 
-}
+    int extraNumber(int a, int b, int c) {
+
+        if (a==b) {
+            return c;
+        }
+        if (b==c){
+            return a;
+        }
+       else
+            return b;
+        }
+
+    }
