c3125e623d3424b31c37401ee730011e790ab0bc
Paulucha <paulina.przekop@gmail.com>
2018-11-07 17:58:25 +0100

    first app compilation

--- Java-RuszGlowa/Impreza.java
+++ Java-RuszGlowa/Impreza.java
@@ -1,11 +1,17 @@
 import java.awt.*;
 import java.awt.event.*;
 class Impreza {
-public static void createInvitation(){
+
+public static void main(String[] args){
+System.out.println("Siemmnako");
+invitation();
+}
+public static void invitation(){
 Frame f = new Frame();
 Label l = new Label ("Party party!");
 Button b = new Button("I will attend");
 Button c = new Button ("no way, i am busy");
 Panel p  = new Panel();
 p.add(l);
-}}
+}
+}
