aa95238ed024c3f1e14814a225092229c67befce
Paulucha <paulina.przekop@gmail.com>
2018-11-07 17:32:23 +0100

    added Java-RuszGlowa file

---
+++ Java-RuszGlowa/Impreza.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+class Impreza {
+public static void createInvitation(){
+Frame f = new Frame();
+Label l = new Label ("Party party!");
+Button b = new Button("I will attend");
+Button c = new Button ("no way, i am busy");
+Panel p  = new Panel();
+p.add(l);
+}}
