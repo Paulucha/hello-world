35af98689ff10f8a321575b474be52dd7293153c
Paulucha <paulina.przekop@gmail.com>
2018-11-13 11:57:03 +0100

    update readme file

--- CodeSignal excercises/src/zadaniaRekrutacyjne/solutions.java
+++ CodeSignal excercises/src/zadaniaRekrutacyjne/solutions.java
@@ -62,7 +62,7 @@ public class solutions {
     }
 
     private static void print(int a, int b) {
-    System.out.println(a + " " + b);
+        System.out.println(a + " " + b);
     }
 }
 
@@ -82,4 +82,155 @@ class MySingleton {
         }
         return SINGLETON_INSTANCE;
     }
+
+
+    public boolean myEquals(String s1, String s2) {
+
+
+        if (s1.equals(null) || s2.equals(null)) {
+            return false;
+        }
+
+
+        if (s1.equals(s2)) {
+            return true;
+        } else return false;
+
+    }
+    public static class Static
+
+    {
+
+        {
+
+            int x = 5;
+            boolean a;
+
+            if ( a = true){
+
+        }
+
+        }
+if (a = true)
+
+        static int x,y;
+
+        public static void main(String args[])
+
+        {
+
+            x--; myMethod();
+
+            System.out.println(x + y + ++x);
+
+        }
+
+        public static void myMethod()
+
+        {
+
+            y = x++ + ++x;
+
+        }
+
+    }
+
+    public static class Test {
+
+        static int age;
+
+        public static void main (String args []) {
+
+            age = age + 1;
+
+            System.out.println("The age is " + age);
+
+        }
+
+    }
+
+    public boolean myEquals(String s1, String s2) {
+
+
+
+        if (s1.equals(null) || s2.equals(null)) {
+            return false;
+        }
+
+        String str1 = s1.replaceAll(" ","");
+        String str2 = s2.replaceAll(" ","");
+
+        if (str1.equals(str2)) {
+            return true;
+        } else return false;
+
+    }
+
+    class MyThread extends Thread
+02
+    {
+        03
+        public void run()
+        04
+        {
+            05
+            System.out.println("MyThread: run()");
+            06
+        }
+        07
+        public void start()
+        08
+        {
+            09
+            System.out.println("MyThread: start()");
+            10
+        }
+        11
+    }
+12
+    class MyRunnable implements Runnable
+13
+    {
+        14
+        public void run()
+        15
+        {
+            16
+            System.out.println("MyRunnable: run()");
+            17
+        }
+        18
+        public void start()
+        19
+        {
+            20
+            System.out.println("MyRunnable: start()");
+            21
+        }
+        22
+    }
+23
+    public class MyTest
+24
+    {
+        25
+        public static void main(String args[])
+        26
+        {
+            27
+            MyThread myThread  =  new MyThread();
+            28
+            MyRunnable myRunnable = new MyRunnable();
+            29
+            Thread thread  =  new Thread(myRunnable);
+            30
+            myThread.start();
+            31
+            thread.start();
+            32
+        }
+
+    }
+
+
 }
--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 This is my exercise repository. Here I add all the courses and exercises that I did for expand of my knowledge about java.
 
-##### Edit:
-Here I am practising my JAVA with **codesignal**.
+- Here I am practising my JAVA with **codesignal**.
 [This is my profile there: https://app.codesignal.com/profile/paulucha ](https://app.codesignal.com/profile/paulucha "/paulucha")
 
-and doing courses at Udemy platform. 
+- and doing courses at **Udemy** platform. 
 
-
+- and studying java following book **Head First Java.**
 
 
 ---
